c8cfbea6353dd876470f30b417492d5d375211e7
Richard Czechowski <richardczechowski@gmail.com>
2021-04-01 09:21:26 -0600

    setup simple reproduction for dangerouslysetinnerhtml

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledDiv = styled.div``
+
+// Uncomment this one and comment the one below to see error
+
+// export const App = ()=>{
+//     return <div
+//         dangerouslySetInnerHTML={{__html: 'foo'}}  // This will be detected
+//     ></div>
+// }
+export const App = ()=>{
+    return <StyledDiv
+        dangerouslySetInnerHTML={{__html: 'foo'}}  // This will not be detected
+    ></StyledDiv>
 }
 
 export default App;
